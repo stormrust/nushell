fe535a69f85f2ac1678a2bc7df281a78584828b9
Michael Angerman <stormasm@gmail.com>
2021-02-12 19:39:31 -0800

    adding in my lex tests from older branch which I am going to delete

--- crates/nu-parser/src/lex/tests.rs
+++ crates/nu-parser/src/lex/tests.rs
@@ -359,3 +359,59 @@ echo 42
         ])
     );
 }
+
+#[test]
+fn command_on_one_line() {
+    let code = r#"
+def my_echo [arg] { echo $arg }
+"#;
+    let (result, err) = lex(code, 0);
+    println!("{:?}", result);
+    assert!(err.is_none());
+    let (result, err) = parse_block(result);
+    assert!(err.is_none());
+    println!("{:?}", result);
+}
+
+#[test]
+fn command_on_two_lines() {
+    let code = r#"
+def my_echo [arg] {
+echo $arg }
+"#;
+    let (result, err) = lex(code, 0);
+    println!("{:?}", result);
+    assert!(err.is_none());
+    let (result, err) = parse_block(result);
+    assert!(err.is_none());
+    println!("{:?}", result);
+}
+
+#[test]
+fn command_on_three_lines() {
+    let code = r#"
+def my_echo [arg] {
+echo $arg
+}
+"#;
+    let (result, err) = lex(code, 0);
+    println!("{:?}", result);
+    assert!(err.is_none());
+    let (result, err) = parse_block(result);
+    assert!(err.is_none());
+    println!("{:?}", result);
+}
+
+#[test]
+fn command_with_parens_broken() {
+    let code = r#"
+def my_echo [arg]
+{ echo $arg }
+"#;
+    let (result, err) = lex(code, 0);
+    println!("{:?}", result);
+    assert!(err.is_none());
+    let (result, err) = parse_block(result);
+    assert!(err.is_none());
+    println!("{:?}", result);
+}
