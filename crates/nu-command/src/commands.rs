eefdfedba1904fae72e4addf0fcb1850a7ce36f3
Michael Angerman <stormasm@gmail.com>
2021-02-24 22:12:42 -0800

    update commands.rs with minimized command set

--- crates/nu-command/src/commands.rs
+++ crates/nu-command/src/commands.rs
@@ -31,19 +31,24 @@ pub(crate) mod debug;
 pub(crate) mod def;
 pub(crate) mod default;
 pub mod default_context;
+/*
 pub(crate) mod describe;
 pub(crate) mod do_;
 pub(crate) mod drop;
 pub(crate) mod du;
 pub(crate) mod each;
+*/
 pub(crate) mod echo;
+/*
 pub(crate) mod empty;
 pub(crate) mod enter;
 pub(crate) mod every;
 pub(crate) mod exec;
+*/
 pub(crate) mod exit;
-pub(crate) mod first;
+//pub(crate) mod first;
 pub(crate) mod flatten;
+/*
 pub(crate) mod format;
 pub(crate) mod from;
 pub(crate) mod from_csv;
@@ -60,34 +65,44 @@ pub(crate) mod from_vcf;
 pub(crate) mod from_xlsx;
 pub(crate) mod from_xml;
 pub(crate) mod from_yaml;
+*/
 pub(crate) mod get;
+pub(crate) mod help;
+/*
 pub(crate) mod group_by;
 pub(crate) mod group_by_date;
 pub(crate) mod hash_;
 pub(crate) mod headers;
-pub(crate) mod help;
+
 pub(crate) mod histogram;
+*/
 pub(crate) mod history;
+/*
 pub(crate) mod if_;
 pub(crate) mod insert;
 pub(crate) mod into_int;
 pub(crate) mod keep;
 pub(crate) mod last;
+*/
 pub(crate) mod let_;
 pub(crate) mod let_env;
-pub(crate) mod lines;
+//pub(crate) mod lines;
 pub(crate) mod ls;
+/*
 pub(crate) mod math;
 pub(crate) mod merge;
 pub(crate) mod mkdir;
 pub(crate) mod move_;
 pub(crate) mod next;
 pub(crate) mod nth;
-pub(crate) mod nu;
+
 pub(crate) mod open;
 pub(crate) mod parse;
 pub(crate) mod path;
+*/
+pub(crate) mod nu;
 pub(crate) mod pivot;
+/*
 pub(crate) mod prepend;
 pub(crate) mod prev;
 pub(crate) mod pwd;
@@ -100,7 +115,7 @@ pub(crate) mod reverse;
 pub(crate) mod rm;
 pub(crate) mod roll;
 pub(crate) mod rotate;
-pub(crate) mod run_external;
+
 pub(crate) mod save;
 pub(crate) mod select;
 pub(crate) mod seq;
@@ -111,11 +126,15 @@ pub(crate) mod size;
 pub(crate) mod skip;
 pub(crate) mod sleep;
 pub(crate) mod sort_by;
-pub(crate) mod source;
+
 pub(crate) mod split;
 pub(crate) mod split_by;
 pub(crate) mod str_;
+*/
+pub(crate) mod run_external;
+pub(crate) mod source;
 pub(crate) mod table;
+/*
 pub(crate) mod tags;
 pub(crate) mod termsize;
 pub(crate) mod to;
@@ -128,24 +147,30 @@ pub(crate) mod to_tsv;
 pub(crate) mod to_url;
 pub(crate) mod to_xml;
 pub(crate) mod to_yaml;
-pub(crate) mod uniq;
+
 pub(crate) mod update;
 pub(crate) mod url_;
-pub(crate) mod version;
+
 pub(crate) mod where_;
 pub(crate) mod which_;
-pub(crate) mod with_env;
-pub(crate) mod wrap;
 
+pub(crate) mod wrap;
+*/
+pub(crate) mod uniq;
+pub(crate) mod version;
+pub(crate) mod with_env;
 pub(crate) use autoview::Autoview;
 pub(crate) use cd::Cd;
 
+/*
 pub(crate) use ansi::Ansi;
 pub(crate) use ansi::AnsiStrip;
 pub(crate) use append::Command as Append;
+*/
 pub(crate) use autoenv::Autoenv;
 pub(crate) use autoenv_trust::AutoenvTrust;
 pub(crate) use autoenv_untrust::AutoenvUnTrust;
+/*
 pub(crate) use benchmark::Benchmark;
 pub(crate) use build_string::BuildString;
 pub(crate) use cal::Cal;
@@ -158,8 +183,10 @@ pub(crate) use config::{
 pub(crate) use count::Count;
 pub(crate) use cp::Cpy;
 pub(crate) use date::{Date, DateFormat, DateListTimeZone, DateNow, DateToTable, DateToTimeZone};
+*/
 pub(crate) use debug::Debug;
 pub(crate) use def::Def;
+/*
 pub(crate) use default::Default;
 pub(crate) use describe::Describe;
 pub(crate) use do_::Do;
@@ -168,22 +195,29 @@ pub(crate) use du::Du;
 pub(crate) use each::Each;
 pub(crate) use each::EachGroup;
 pub(crate) use each::EachWindow;
-pub(crate) use echo::Echo;
+
 pub(crate) use empty::Command as Empty;
 pub(crate) use if_::If;
-pub(crate) use nu::NuPlugin;
+
 pub(crate) use update::Command as Update;
 pub(crate) mod kill;
 pub(crate) use kill::Kill;
+*/
+pub(crate) use echo::Echo;
+pub(crate) use nu::NuPlugin;
 pub(crate) mod clear;
 pub(crate) use clear::Clear;
 pub(crate) mod touch;
+/*
 pub(crate) use enter::Enter;
 pub(crate) use every::Every;
 pub(crate) use exec::Exec;
+*/
 pub(crate) use exit::Exit;
-pub(crate) use first::First;
 pub(crate) use flatten::Command as Flatten;
+/*
+pub(crate) use first::First;
+
 pub(crate) use format::{FileSize, Format};
 pub(crate) use from::From;
 pub(crate) use from_csv::FromCSV;
@@ -201,22 +235,29 @@ pub(crate) use from_xlsx::FromXLSX;
 pub(crate) use from_xml::FromXML;
 pub(crate) use from_yaml::FromYAML;
 pub(crate) use from_yaml::FromYML;
+*/
 pub(crate) use get::Command as Get;
+/*
 pub(crate) use group_by::Command as GroupBy;
 pub(crate) use group_by_date::GroupByDate;
 pub(crate) use hash_::{Hash, HashBase64};
 pub(crate) use headers::Headers;
+*/
 pub(crate) use help::Help;
-pub(crate) use histogram::Histogram;
+
+//pub(crate) use histogram::Histogram;
 pub(crate) use history::History;
+/*
 pub(crate) use insert::Command as Insert;
 pub(crate) use into_int::IntoInt;
 pub(crate) use keep::{Keep, KeepUntil, KeepWhile};
 pub(crate) use last::Last;
+*/
 pub(crate) use let_::Let;
 pub(crate) use let_env::LetEnv;
-pub(crate) use lines::Lines;
+//pub(crate) use lines::Lines;
 pub(crate) use ls::Ls;
+/*
 pub(crate) use math::{
     Math, MathAbs, MathAverage, MathCeil, MathEval, MathFloor, MathMaximum, MathMedian,
     MathMinimum, MathMode, MathProduct, MathRound, MathStddev, MathSummation, MathVariance,
@@ -232,7 +273,9 @@ pub(crate) use path::{
     PathBasename, PathCommand, PathDirname, PathExists, PathExpand, PathExtension, PathFilestem,
     PathType,
 };
+*/
 pub(crate) use pivot::Pivot;
+/*
 pub(crate) use prepend::Prepend;
 pub(crate) use prev::Previous;
 pub(crate) use pwd::Pwd;
@@ -249,7 +292,7 @@ pub(crate) use reverse::Reverse;
 pub(crate) use rm::Remove;
 pub(crate) use roll::{Roll, RollColumn, RollUp};
 pub(crate) use rotate::{Rotate, RotateCounterClockwise};
-pub(crate) use run_external::RunExternalCommand;
+
 pub(crate) use save::Save;
 pub(crate) use select::Command as Select;
 pub(crate) use seq::Seq;
@@ -260,7 +303,10 @@ pub(crate) use size::Size;
 pub(crate) use skip::{Skip, SkipUntil, SkipWhile};
 pub(crate) use sleep::Sleep;
 pub(crate) use sort_by::SortBy;
+*/
+pub(crate) use run_external::RunExternalCommand;
 pub(crate) use source::Source;
+/*
 pub(crate) use split::{Split, SplitChars, SplitColumn, SplitRow};
 pub(crate) use split_by::SplitBy;
 pub(crate) use str_::{
@@ -269,7 +315,9 @@ pub(crate) use str_::{
     StrReverse, StrScreamingSnakeCase, StrSnakeCase, StrStartsWith, StrSubstring, StrToDatetime,
     StrToDecimal, StrToInteger, StrTrim, StrTrimLeft, StrTrimRight, StrUpcase,
 };
+*/
 pub(crate) use table::Table;
+/*
 pub(crate) use tags::Tags;
 pub(crate) use termsize::TermSize;
 pub(crate) use to::To;
@@ -282,15 +330,19 @@ pub(crate) use to_tsv::ToTSV;
 pub(crate) use to_url::ToURL;
 pub(crate) use to_xml::ToXML;
 pub(crate) use to_yaml::ToYAML;
-pub(crate) use touch::Touch;
-pub(crate) use uniq::Uniq;
+
+
 pub(crate) use url_::{UrlCommand, UrlHost, UrlPath, UrlQuery, UrlScheme};
-pub(crate) use version::Version;
+
 pub(crate) use where_::Where;
 pub(crate) use which_::Which;
-pub(crate) use with_env::WithEnv;
-pub(crate) use wrap::Wrap;
 
+pub(crate) use wrap::Wrap;
+*/
+pub(crate) use touch::Touch;
+pub(crate) use uniq::Uniq;
+pub(crate) use version::Version;
+pub(crate) use with_env::WithEnv;
 #[cfg(test)]
 mod tests {
     use super::*;
